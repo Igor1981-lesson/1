0ad0255e228d1c5a7f5307d667522eb028de8756
Igor <sitesbuildbk@gmail.com>
2021-01-07 16:29:55 +0200

    add script2.js

--- js/script.js
+++ js/script.js
@@ -2,27 +2,64 @@
 
 
 
-let number = 5;
-const leftBorderWidth = 1;
-number = 10;
+// let number = 5;
+// const leftBorderWidth = 1;
+// number = 10;
 
-console.log(number);
-const obj = {
-    a: 50
-};
-obj.a = 10;
-console.log(obj.a);
+// console.log(number);
+// const obj = {
+//     a: 50
+// };
+// obj.a = 10;
+// console.log(obj.a);
 //alert("Hello");
 // const result = confirm("Are you here?");
 // console.log(result);
 // const answer = +prompt("Are toy 18?", "18");
 // console.log(answer);
-const answers =[];
+// const answers =[];
 // answers[0]= prompt('what is your name?', '');
 // answers[1]= prompt('what is your lastname?', '');
 // answers[2]= prompt('how old are you?', '');
-console.log(typeof(null));
-const category = 'toys';
-alert(`http://someurl.com/${category}/5filter=23`);
-const user = 'Igor';
-alert(`Hello, ${user}`);
+// console.log(typeof(null));
+
+// интерполяция
+// const category = 'toys';
+// alert(`http://someurl.com/${category}/5filter=23`);
+// const user = 'Igor';
+// alert(`Hello, ${user}`);
+
+// операторы в js
+// console.log('arr '+"- object");
+// console.log(4 + "- object");
+// console.log(4 +"5");
+
+// let incr = 10,
+//     decr = 10;
+// incr++;
+// decr--;
+// ++incr;
+// --decr;
+// console.log(++incr);
+// console.log(--decr);
+// остаток от деления двух чисел %
+// console.log(5%2);
+// console.log(2*4 == 8);
+// console.log(2*4 === "8");
+
+// and &&
+// const isChecked = true,
+//         isClose = false;
+//         console.log(isChecked && isClose);
+// //or ||
+// const isChecked = true,
+//         isClose = false;
+//         console.log(isChecked || isClose);
+// // not !
+// const isChecked = true,
+//         isClose = false;
+//         console.log(isChecked && !isClose);
+
+// система контроля
+
+
