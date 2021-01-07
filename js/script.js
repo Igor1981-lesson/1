6a0ac853f6903c1927f4cf42e16e9c156a493cae
Igor <sitesbuildbk@gmail.com>
2021-01-07 15:54:38 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+
+let number = 5;
+const leftBorderWidth = 1;
+number = 10;
+
+console.log(number);
+const obj = {
+    a: 50
+};
+obj.a = 10;
+console.log(obj.a);
+//alert("Hello");
+// const result = confirm("Are you here?");
+// console.log(result);
+// const answer = +prompt("Are toy 18?", "18");
+// console.log(answer);
+const answers =[];
+// answers[0]= prompt('what is your name?', '');
+// answers[1]= prompt('what is your lastname?', '');
+// answers[2]= prompt('how old are you?', '');
+console.log(typeof(null));
+const category = 'toys';
+alert(`http://someurl.com/${category}/5filter=23`);
+const user = 'Igor';
+alert(`Hello, ${user}`);
